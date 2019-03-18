45c76ad91a4aa7d79df0b92f52dca04b967078ed
Stepan Pesternikov <s.pesternikov@dlg.im>
2019-03-18 23:55:26 +0300

    feat(kafka) change kafka driver

--- kafka/consumer/consumer.go
+++ kafka/consumer/consumer.go
@@ -140,8 +140,8 @@ func (c *Consumer) Start() error {
 
 func (c *Consumer) Stop() {
 	c.cancel()
-	c.reader.Close()
 	c.wg.Wait()
+	c.reader.Close()
 }
 
 func (c *Consumer) commitLoop() {
@@ -282,6 +282,7 @@ func (c *Consumer) readNext() (*kafka.Message, bool) {
 				if typedErr.Code() == kafka.ErrTimedOut {
 					select {
 					case <-c.ctx.Done():
+						return nil, false
 					default:
 						continue
 					}
