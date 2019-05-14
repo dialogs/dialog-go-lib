bcfa2a507940d5bb4eea8d15971cc5c6d856f2df
khevse <khevse@gmail.com>
2019-05-14 13:29:16 +0300

    feat (database): add database migration package with embedded assets file

---
+++ db/migrations/assets_manager_test.go
@@ -0,0 +1,70 @@
+package migrations
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/dialogs/dialog-go-lib/db/migrations/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestManager(t *testing.T) {
+
+	list, err := GetFilesList(test.Assets, test.DirName)
+	require.NoError(t, err)
+	require.Contains(t, list, "1_example.down.sql")
+	require.Contains(t, list, "1_example.up.sql")
+
+	fn := NewFileReaderHandler(test.Assets, test.DirName)
+	data, err := fn("1_example.down.sql")
+	require.NoError(t, err)
+
+	require.Equal(t, "DROP TABLE example;", string(data))
+
+}
+
+func TestDriver(t *testing.T) {
+
+	// test: fail
+	driver, err := NewAssetsDriver(test.Assets, test.DirName+"1", GetFilesList)
+	require.EqualError(t,
+		err,
+		"files for migrations assets driver: failed to open directory '/assets1': file does not exist")
+	require.Nil(t, driver)
+
+	// test: ok
+	driver, err = NewAssetsDriver(test.Assets, test.DirName, GetFilesList)
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, driver.Close())
+	}()
+
+	version, err := driver.First()
+	require.NoError(t, err)
+
+	{
+		// test: up
+		reader, identifier, err := driver.ReadUp(version)
+		require.NoError(t, err)
+		require.Equal(t, "example", identifier)
+
+		b, err := ioutil.ReadAll(reader)
+		require.NoError(t, err)
+		require.Equal(t,
+			"CREATE TABLE example\n(\n    id uuid NOT NULL PRIMARY KEY,\n    value varchar(1) NOT NULL\n);\n",
+			string(b))
+	}
+
+	{
+		// test: down
+		reader, identifier, err := driver.ReadDown(version)
+		require.NoError(t, err)
+		require.Equal(t, "example", identifier)
+
+		b, err := ioutil.ReadAll(reader)
+		require.NoError(t, err)
+		require.Equal(t,
+			"DROP TABLE example;",
+			string(b))
+	}
+}
---
+++ db/migrations/test/assets/1_example.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE example
+(
+    id uuid NOT NULL PRIMARY KEY,
+    value varchar(1) NOT NULL
+);
