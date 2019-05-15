69b200476f3a0a272ba5294636477df8c9ab30fd
khevse <khevse@gmail.com>
2019-05-15 11:07:20 +0300

    feat (config): add package config

---
+++ config/helper.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// GetString returns string value. Returns error if value is not set
+func GetString(src *viper.Viper, key string) (string, error) {
+
+	if src.IsSet(key) {
+		return src.GetString(key), nil
+	}
+
+	return "", newError(key)
+}
+
+// GetDuration returns duration value. Returns error if value is not set
+func GetDuration(src *viper.Viper, key string) (time.Duration, error) {
+
+	if src.IsSet(key) {
+		return src.GetDuration(key), nil
+	}
+
+	return 0, newError(key)
+}
+
+// GetBool returns boolean value. Returns error if value is not set
+func GetBool(src *viper.Viper, key string) (bool, error) {
+
+	if src.IsSet(key) {
+		return src.GetBool(key), nil
+	}
+
+	return false, newError(key)
+}
+
+// GetInt returns integer value. Returns error if value is not set
+func GetInt(src *viper.Viper, key string) (int, error) {
+
+	if src.IsSet(key) {
+		return src.GetInt(key), nil
+	}
+
+	return 0, newError(key)
+}
+
+// GetFloat64 returns float64 value. Returns error if value is not set
+func GetFloat64(src *viper.Viper, key string) (float64, error) {
+
+	if src.IsSet(key) {
+		return src.GetFloat64(key), nil
+	}
+
+	return 0, newError(key)
+}
+
+func newError(key string) error {
+	return fmt.Errorf("not found config value: '%s'", key)
+}
